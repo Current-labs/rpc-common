142e4c56b198a0570e20034398686a5d75acc3fb
Zernyu Chou <zernyu@gmail.com>
2020-08-21 15:15:15 -0600

    Moves function for easier merging

--- src/mg_rpc_channel_http.c
+++ src/mg_rpc_channel_http.c
@@ -284,44 +284,6 @@ static bool mg_rpc_channel_http_send_frame(struct mg_rpc_channel *ch,
   return true;
 }
 
-struct mg_rpc_channel *mg_rpc_channel_http(struct mg_connection *nc,
-                                           const char *default_auth_domain,
-                                           const char *default_auth_file) {
-  struct mg_rpc_channel *ch = (struct mg_rpc_channel *) calloc(1, sizeof(*ch));
-  ch->ch_connect = mg_rpc_channel_http_ch_connect;
-  ch->send_frame = mg_rpc_channel_http_send_frame;
-  ch->ch_close = mg_rpc_channel_http_ch_close;
-  ch->ch_destroy = mg_rpc_channel_http_ch_destroy;
-  ch->get_type = mg_rpc_channel_http_get_type;
-  /*
-   * New channel is created for each incoming HTTP request, so the channel
-   * is not persistent.
-   *
-   * Rationale for this behaviour, instead of updating channel's destination on
-   * each incoming frame, is that this won't work with asynchronous responses.
-   */
-  ch->is_persistent = mg_rpc_channel_false;
-  /*
-   * HTTP channel expects exactly one response.
-   * We don't want random broadcasts to be sent as a response.
-   */
-  ch->is_broadcast_enabled = mg_rpc_channel_false;
-  ch->get_authn_info = mg_rpc_channel_http_get_authn_info;
-  ch->cors_preflight_check = mg_rpc_channel_http_cors_preflight_check; 
-  ch->send_cors_preflight = mg_rpc_channel_http_send_cors_preflight;
-  ch->send_not_authorized = mg_rpc_channel_http_send_not_authorized;
-  ch->get_info = mg_rpc_channel_http_get_info;
-
-  struct mg_rpc_channel_http_data *chd =
-      (struct mg_rpc_channel_http_data *) calloc(1, sizeof(*chd));
-  chd->default_auth_domain = default_auth_domain;
-  chd->default_auth_file = default_auth_file;
-  chd->mgr = nc->mgr;
-  ch->channel_data = chd;
-  nc->user_data = ch;
-  return ch;
-}
-
 void mg_rpc_channel_http_recd_frame(struct mg_connection *nc,
                                     struct http_message *hm,
                                     struct mg_rpc_channel *ch,
@@ -357,6 +319,44 @@ void mg_rpc_channel_http_recd_parsed_frame(struct mg_connection *nc,
   /* "Open" the channel and send the frame */
   ch->ev_handler(ch, MG_RPC_CHANNEL_OPEN, NULL);
   ch->ev_handler(ch, MG_RPC_CHANNEL_FRAME_RECD_PARSED, &frame);
+}
+
+struct mg_rpc_channel *mg_rpc_channel_http(struct mg_connection *nc,
+                                           const char *default_auth_domain,
+                                           const char *default_auth_file) {
+  struct mg_rpc_channel *ch = (struct mg_rpc_channel *) calloc(1, sizeof(*ch));
+  ch->ch_connect = mg_rpc_channel_http_ch_connect;
+  ch->send_frame = mg_rpc_channel_http_send_frame;
+  ch->ch_close = mg_rpc_channel_http_ch_close;
+  ch->ch_destroy = mg_rpc_channel_http_ch_destroy;
+  ch->get_type = mg_rpc_channel_http_get_type;
+  /*
+   * New channel is created for each incoming HTTP request, so the channel
+   * is not persistent.
+   *
+   * Rationale for this behaviour, instead of updating channel's destination on
+   * each incoming frame, is that this won't work with asynchronous responses.
+   */
+  ch->is_persistent = mg_rpc_channel_false;
+  /*
+   * HTTP channel expects exactly one response.
+   * We don't want random broadcasts to be sent as a response.
+   */
+  ch->is_broadcast_enabled = mg_rpc_channel_false;
+  ch->get_authn_info = mg_rpc_channel_http_get_authn_info;
+  ch->cors_preflight_check = mg_rpc_channel_http_cors_preflight_check;
+  ch->send_cors_preflight = mg_rpc_channel_http_send_cors_preflight;
+  ch->send_not_authorized = mg_rpc_channel_http_send_not_authorized;
+  ch->get_info = mg_rpc_channel_http_get_info;
+
+  struct mg_rpc_channel_http_data *chd =
+      (struct mg_rpc_channel_http_data *) calloc(1, sizeof(*chd));
+  chd->default_auth_domain = default_auth_domain;
+  chd->default_auth_file = default_auth_file;
+  chd->mgr = nc->mgr;
+  ch->channel_data = chd;
+  nc->user_data = ch;
+  return ch;
 }
 
 #endif /* defined(MGOS_HAVE_HTTP_SERVER) && MGOS_ENABLE_RPC_CHANNEL_HTTP */
